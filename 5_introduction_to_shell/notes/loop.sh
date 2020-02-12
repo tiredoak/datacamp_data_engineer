6356d839cfe6cbb7d3d99aec90dc298934df1bb6
miguelccarvalho <miguel@theaso.co>
2020-02-12 09:23:47 +0000

    finished introduction to shell notes

---
+++ 5_introduction_to_shell/notes/loop.sh
@@ -0,0 +1,6 @@
+# Loop to print the head and tail of each file
+for filename in $@
+do
+	cut -d , -f 1 $filename | grep -v Date | sort | head -n 1
+	cut -d , -f 1 $filename | grep -v Date | sort | tail -n 1
+done
---
+++ 5_introduction_to_shell/notes/single_paramenters.sh
@@ -0,0 +1,1 @@
+cut -d , -f $1 $3 | grep -v $2 | sort -n -r
